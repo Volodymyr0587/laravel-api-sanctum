d71324e07cefd3c2b29dc1fdba512ef09d9cdd1e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-23 15:01:40 +0200

    Migration for Task model, relationship BelongsTo between User and Task models

--- database/migrations/2024_02_22_191151_create_tasks_table.php
+++ database/migrations/2024_02_22_191151_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description');
+            $table->string('priority')->default('medium');
             $table->timestamps();
         });
     }
